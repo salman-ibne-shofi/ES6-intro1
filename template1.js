e602c0ce04c02d9433afd07ce56c33639a0f11c3
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-04-30 04:31:11 +0600

    template string

---
+++ template1.js
@@ -0,0 +1,19 @@
+const first = "jaan";
+const last = "pakhi";
+const greet = "potas potas";
+const name = first + " " + last + " " + greet;
+console.log(name);
+const a = 10;
+const b = 20;
+const result = "the sum of " + a + " and " + b + " is " + (a + b);
+console.log(result);
+const math = `the sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+const email = "Hi john \n" + "cena cena lage \n" + "tumi mara mari korba naki";
+console.log(email);
+
+const challenge = `john cena
+cena naki ochena 
+mair dimu prochur`;
+console.log(challenge);
